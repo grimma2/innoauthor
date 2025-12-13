593803e24da72015b3a77e6ebb808f3f6bc04811
Shushpanov-Live <shushpanov-official@yandex.ru>
2025-12-14 01:37:52 +0300

    <любое сообщение о коммите>

--- InnAuthor/settings.py
+++ InnAuthor/settings.py
@@ -30,7 +30,9 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
-DEBUG = os.getenv('DEBUG', 'False').strip().lower() in ['true', '1', 'yes']
+#DEBUG = os.getenv('DEBUG', 'False').strip().lower() in ['true', '1', 'yes']
+DEBUG = True
+
 
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(' ')
